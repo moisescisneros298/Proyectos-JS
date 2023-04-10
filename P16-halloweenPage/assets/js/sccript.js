4969da7f82ed2cf61f4a79eb1ba64123c3fc2331
moises <moisescisneros298@gmail.com>
2023-04-10 13:36:17 -0600

    P16 js scroll reveal

--- P16-halloweenPage/assets/js/sccript.js
+++ P16-halloweenPage/assets/js/sccript.js
@@ -12,3 +12,20 @@ if(navClose){
         navMenu.classList.remove("show-menu")
     })
 }
+
+
+
+// Scroll reveal
+const sr = ScrollReveal({
+    origin:"top",
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+sr.reveal(`.home-swiper,.new-swiper, .newsletter__container`)
+sr.reveal(`.category__data, .trick__content, .footer__content`, {
+    interval:100
+})
+sr.reveal(`.about__data, .discount__img`,{origin: "left"})
+sr.reveal(`.about__img, .discount__data`,{origin: "right"})
