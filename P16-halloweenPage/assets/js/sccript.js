bc0a42f66296d80faa70d365bbc1d012927a5503
moises <moisescisneros298@gmail.com>
2023-04-11 11:01:16 -0600

    P16 boton para volver al inicio

--- P16-halloweenPage/assets/js/sccript.js
+++ P16-halloweenPage/assets/js/sccript.js
@@ -30,6 +30,15 @@ let homeSwiper = new Swiper(".home-swiper",{
     }
 })
 
+// scroll up
+function scrollUp() {
+    const scrollup = document.getElementById("scroll-up")
+    if(this.scrollY>=460) scrollup.classList.add('show-scroll'); else scrollup.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollUp)
+
+
+
 let newSwiper = new Swiper(".new-swiper", {
     centeredSlides: true,
     slidesPerView:"auto",
