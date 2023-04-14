3dedc6c2194c718721020ea7d2bb25643f4c6c70
moises <moisescisneros298@gmail.com>
2023-04-14 11:28:34 -0600

    P16 Final

--- P16-halloweenPage/assets/js/sccript.js
+++ P16-halloweenPage/assets/js/sccript.js
@@ -46,13 +46,30 @@ let newSwiper = new Swiper(".new-swiper", {
     spaceBetween: 16,
 })
 
+// Section active
+const sections = document.querySelectorAll("section[id]")
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current => {
+        const sectionHeght = current.offsetHeight,
+        sectionTop = current.offsetTop -58,
+        sectionId = current.getAttribute("id")
+        if(scrollY>sectionTop && scrollY<=sectionTop+sectionHeght){
+            document.querySelector(".nav__menu a[href*=" + sectionId+"]").classList.add("active-link")
+        }else {
+            document.querySelector(".nav__menu a[href*=" + sectionId+"]").classList.remove("active-link")
+        }
+    })
+}
+window.addEventListener("scroll",scrollActive)
+
+
 // Scroll reveal
 const sr = ScrollReveal({
     origin:"top",
     distance: '60px',
     duration: 2500,
     delay: 400,
-    reset: true
 })
 sr.reveal(`.home-swiper,.new-swiper, .newsletter__container`)
 sr.reveal(`.category__data, .trick__content, .footer__content`, {
