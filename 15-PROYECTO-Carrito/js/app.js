33dbe41a401d8ff0af22018670d37d7f77740ae8
moises <moisescisneros298@gmail.com>
2023-05-20 13:17:19 -0600

    Leer los datos del curso seleccionado

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCurso = document.querySelector('#lista-cursos');
+let articulosCarrito = []; 
 
 cargarEventListeneers();
 function cargarEventListeneers() {
@@ -15,6 +16,24 @@ function agregarCurso(e) {
     e.preventDefault();
 
     if( e.target.classList.contains('agregar-carrito') ) {
-        console.log('agregando al carrito---')
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado)
     }
+}
+
+// Leer el contenido del HTML al que le dimos click y extrae la informacion del curso
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCurso)
 }
