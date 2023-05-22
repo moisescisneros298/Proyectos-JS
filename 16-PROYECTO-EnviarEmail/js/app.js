6e165ac41b251c8ab19fcdb1b67c60adbd5db6d4
moises <moisescisneros298@gmail.com>
2023-05-22 13:28:22 -0600

    Habiltar y deshabilitar el enviar

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,28 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     // Seleccionar los elementos de la interfas
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
-    const formulario = document.querySelector('#formulario')
+    const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', validar );
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar );
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
 
     function validar (e) {
         if(e.target.value.trim() === ''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail()
             return;
         }
 
         if(e.target.id === 'email' && !validarEmail(e.target.value)){
             mostrarAlerta('El email no es valido', e.target.parentElement)
+            email[e.target.name] = '';
+            comprobarEmail()
             return;
         }
 
         limpiarAlerta(e.target.parentElement);
+
+        // Asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        //  Comprobar el objeto de email
+        comprobarEmail();
     }
     function mostrarAlerta(mensaje, referencia){
         limpiarAlerta(referencia)
@@ -48,5 +65,15 @@ document.addEventListener('DOMContentLoaded', function() {
         const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
         const resultado = regex.test(email);
         return resultado;
+    }
+
+    function comprobarEmail() {
+        if( Object.values(email).includes('') ){
+            btnSubmit.classList.add('opacity-50' );
+            btnSubmit.disabled = true;
+            return;
+        }
+        btnSubmit.classList.remove('opacity-50' );
+        btnSubmit.disabled = false;
     }
 })
