efc8095d35b66677667367855605a3047759dc73
moises <moisescisneros298@gmail.com>
2023-05-22 11:39:05 -0600

    Añadiendo alerta al HTML

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario')
 
     // Asignar eventos
     inputEmail.addEventListener('blur', validar );
@@ -12,9 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function validar (e) {
         if(e.target.value.trim() === ''){
-            console.log('Esta vacio');
+            mostrarAlerta();
         } else {
             console.log('si hay algo');
+        }
+
+        function mostrarAlerta(){
+            // Generar alerta en HTML
+            const error = document.createElement('P')
+            error.textContent = 'Hubo un error';
+            error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
+
+            // Inyectar el error al formulario
+            formulario.appendChild(error)
         }
     }
 })
