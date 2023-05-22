b1c2b87da5fcfbdc718d18288911f3ccba2ee729
moises <moisescisneros298@gmail.com>
2023-05-22 12:50:49 -0600

    Validar email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -17,6 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement)
+            return;
+        }
+
         limpiarAlerta(e.target.parentElement);
     }
     function mostrarAlerta(mensaje, referencia){
@@ -37,5 +42,11 @@ document.addEventListener('DOMContentLoaded', function() {
         if(alerta){
             alerta.remove();
         }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        return resultado;
     }
 })
