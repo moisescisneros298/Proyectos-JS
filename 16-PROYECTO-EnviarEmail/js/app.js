6ff69b7ac8f5ee39536351a050ad5b096d1659e7
moises <moisescisneros298@gmail.com>
2023-05-22 11:04:08 -0600

    elementos de la interfas y eventos

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Seleccionar los elementos de la interfas
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', function(e) {
+
+        console.log(e.target.value)
+    });
+})
