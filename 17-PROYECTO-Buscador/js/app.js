d743e2036278d5c53d9d8a6d26bb7a985c11280b
moises <moisescisneros298@gmail.com>
2023-05-25 12:48:18 -0600

    Filtrar por precio

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -45,9 +45,13 @@ year.addEventListener('change', e => {
 });
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
+
+    filtrarAuto();
 });
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value;
+
+    filtrarAuto();
 });
 puertas.addEventListener('change', e => {
     datosBusqueda.puertas = e.target.value;
@@ -97,7 +101,7 @@ function llenarSelect() {
 
 // Funcion que filtra en base a la busqueda 
 function filtrarAuto() {
-    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear )
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter(filtrarMinimo).filter(filtrarMaximo)
     // console.log(resultado)
 
     mostrarAutos(resultado);
@@ -114,6 +118,22 @@ function filtrarYear(auto) {
     const { year } = datosBusqueda;
     if(year){
         return auto.year === parseInt(year);
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto){
+    const { minimo } = datosBusqueda;
+    if(minimo){
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto){
+    const { maximo } = datosBusqueda;
+    if(maximo){
+        return auto.precio <= maximo;
     }
     return auto;
 }
