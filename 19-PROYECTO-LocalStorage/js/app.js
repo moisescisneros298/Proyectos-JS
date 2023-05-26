ebd2974b9ecf263d5f7cc5c6002521a2d9c12bd4
moises <moisescisneros298@gmail.com>
2023-05-26 14:15:02 -0600

    19 Validacion del formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -15,5 +15,31 @@ function eventListeners(){
 function agregarTweet(e){
     e.preventDefault();
 
-    console.log('Agregando tweet');
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validacion 
+    if(tweet === ''){
+        mostrarError('Un mensaje no puede ir vacio')
+        return; //Evita que se ejecuten mas lineas de codigo
+    }
+
+    console.log('agregando tweet')
+}
+
+// Mostrar mensaje de error
+
+function mostrarError(error){
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el HTML
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta despues de 3s
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
