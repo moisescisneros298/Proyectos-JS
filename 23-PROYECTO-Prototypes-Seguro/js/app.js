b56a9752ce5786b6bbb907d296e59de16050dcba
Moises <moisescisneros298@gmail.com>
2023-06-27 12:35:27 -0600

    calculo del seguro1

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -21,6 +21,29 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+//Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo ) => {
+
+    const div = document.createElement('div');
+    
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto')
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje
+
+    // Incertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove()
+    }, 3000)
+}
+
 //Instanciar UI
 const ui = new UI();
 console.log(ui);
@@ -49,9 +72,16 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === '' || year === '' || tipo === '') {
-        console.log('No paso la validacion');
-    } else {
-        console.log('si paso la validacion...')
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
     }
 
+    ui.mostrarMensaje('Cotizando...', 'exito');
+
+    //Instanciar el seguro
+    const seguro = new Seguro(marca,year, tipo);
+
+    console.log(seguro)
+
+    // Utilizar el prototype ue va a cotizar
 }
