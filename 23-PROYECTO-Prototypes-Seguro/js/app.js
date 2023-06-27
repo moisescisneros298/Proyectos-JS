38731e827cf9ef70113066d1779b2bca0b58f178
Moises <moisescisneros298@gmail.com>
2023-06-27 11:36:13 -0600

    Validar formulario 1

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -29,3 +29,29 @@ console.log(ui);
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con los years
 })
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+
+    //Leer el year seleccionado
+    const year = document.querySelector('#year').value;
+
+    //Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === '' || year === '' || tipo === '') {
+        console.log('No paso la validacion');
+    } else {
+        console.log('si paso la validacion...')
+    }
+
+}
