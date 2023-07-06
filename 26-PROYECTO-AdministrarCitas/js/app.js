faa8d0da3a8417755eb0da767203ca5493c70ec2
moises <moisescisneros298@gmail.com>
2023-07-06 15:06:33 -0600

    Mostrar citas en el HTML

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -48,6 +48,65 @@ class UI {
             divMensaje.remove();
         }, 5000);
     }
+
+    imprimirCitas({citas}) {
+
+        this.limpiarHTML();
+
+        citas.forEach( cita => {
+            const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3');
+            divCita.dataset.id = id;
+
+            // Scripting de los elementos de la cita
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-wight-bolder');
+            mascotaParrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement('P');
+            propietarioParrafo.innerHTML = `
+                <span class="font-weight-bolder">Propietario: </span> ${propietario}
+            ` 
+
+            const telefonoParrafo = document.createElement('P');
+            telefonoParrafo.innerHTML = `
+                <span class="font-weight-bolder"> Telefono: </span> ${telefono}
+            `
+            const fechaParrafo = document.createElement('P');
+            fechaParrafo.innerHTML = `
+                <span class="font-weight-bolder"> Fecha: </span> ${fecha}
+            `
+
+            const horaParrafo = document.createElement('P');
+            horaParrafo.innerHTML = `
+                <span class="font-weight-bolder"> Hora: </span> ${hora}
+            `
+
+            const sintomasParrafo = document.createElement('P');
+            sintomasParrafo.innerHTML = `
+                <span class="font-weight-bolder"> Sintomas: </span> ${sintomas}
+            `
+            
+            // Agrehat los parrafos al divCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+
+            // Agregar las citas al HTML
+            contenedorCitas.appendChild(divCita);
+        } )
+    }
+
+    limpiarHTML(){
+        while(contenedorCitas.firstChild) {
+            contenedorCitas.removeChild(contenedorCitas.firstChild)
+        }
+    }
 }
 
 const ui = new UI();
@@ -112,7 +171,7 @@ function nuevaCita(e) {
     formulario.reset();
 
     // Mostrar el HTML de las citas
-    
+    ui.imprimirCitas(administrarCitas);
 }
 
 function reiniciarObjeto() {
