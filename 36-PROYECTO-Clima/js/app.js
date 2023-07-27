44be977380e3cd587b4719b9a68646df4ada5601
moises <moisescisneros298@gmail.com>
2023-07-27 13:08:35 -0600

    Fin del proyecto clima

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -55,6 +55,7 @@ function consultarAPI(ciudad, pais){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId2}`;
     
     console.log(url)
+    Spinner();// muestra un spinner de carga
 
     fetch(url)
         .then( respuesta => respuesta.json())
@@ -112,4 +113,28 @@ function limpiarHTML() {
     while(resultado.firstChild){
         resultado.removeChild(resultado.firstChild)
     }
+}
+
+function Spinner(){
+    limpiarHTML();
+    
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+    resultado.appendChild(divSpinner);
+
 }
