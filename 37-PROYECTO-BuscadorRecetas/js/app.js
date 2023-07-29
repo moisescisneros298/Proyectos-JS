0af718b48b06f3689a8fbffe8ab3e74e5faf688f
moises <moisescisneros298@gmail.com>
2023-07-29 12:05:26 -0600

    Mostrando ingredientes y cantidades

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -4,6 +4,7 @@ function iniciarAPP(){
     selectCategorias.addEventListener('change', selectCategoria);
 
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {})
 
     obtenerCategorias();
 
@@ -93,16 +94,45 @@ function iniciarAPP(){
         const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then(respuesta => console.log(respuesta.meals[0]))
+            .then(respuesta => mostrarRecetaModal(respuesta.meals[0]))
     }
 
     function mostrarRecetaModal(receta){
-        
-    }
 
-    function mostrarRecetaModal(receta){
+        const {idMeal, strInstructions, strMeal, strMealThumb} = receta;
+
+        // Añadir contenido al modal
+        const modalTitle = document.querySelector('.modal .modal-title ');
+        const modalBody = document.querySelector('.modal .modal-body ');
+
+        modalTitle.textContent = strMeal
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades </h3>
+        `;
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-group');
+        // Mostrar cantidades e ingredientes
+            for(let i = 1; i <= 20; i++) {
+                if(receta[`strIngredient${i}`]){
+                    const ingrediente = receta[`strIngredient${i}`];
+                    const cantidad = receta[`strMeasure${i}`];
+
+                    const ingredienteLi = document.createElement('LI');
+                    ingredienteLi.classList.add('list-group-item');
+                    ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+
+                    listGroup.appendChild(ingredienteLi);
+                }
+            }
+
+            modalBody.appendChild(listGroup)
+
         // Muestra el modal
-        modal.show()
+        modal.show();
     }
 
     function limpiarHtml(selector){
