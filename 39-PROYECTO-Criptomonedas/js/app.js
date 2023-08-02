4f0d24b62ceb60e8fedf598b1f8b4e6ed28a33b8
moises <moisescisneros298@gmail.com>
2023-08-02 12:01:00 -0600

    Leer monedas y criptomonedas seleccionadas

---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,60 @@
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
+// Crear un promise
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas) 
+} )
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
+})
+
+function consultarCriptomonedas(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+
+    fetch(url)
+        .then( respuesta => respuesta.json()) 
+        .then( resultado => obtenerCriptomonedas(resultado.Data))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas) )
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const {FullName, Name} = cripto.CoinInfo;
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option)
+    })
+}
+
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e){
+    e.preventDefault();
+
+    // Validar
+    const {moneda, criptomoneda} = objBusqueda;
+
+    if(moneda === '' || criptomoneda === ''){
+        mostrarAlerta('Ambos campos son obligatorios');
+        return
+    }
+}
+
+function mostrarAlerta(mensaje){
+    console.log(mensaje)
+}
