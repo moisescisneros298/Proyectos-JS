6e7d35d2703bffa9f875e46243c7efacc8dc243f
moises <moisescisneros298@gmail.com>
2023-08-08 14:56:54 -0600

    validacion y almacenamiento del formulario

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -15,8 +15,31 @@ function guardarCliente(){
     const camposVacios = [mesa, hora].some( campo => campo === '' );
 
     if(camposVacios){
-        console.log('Si hay almenos un campo vacio');
-    } else {
-        console.log('todos los campos estan llenos')
+        // Verificar si ya hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta){
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            // Eliminar la alerta
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+
+            return;
+        }
     }
+
+    // Asignar datos del formulario a cliente
+    cliente = {...cliente, mesa, hora}
+
+    // Ocultar modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+
 }
